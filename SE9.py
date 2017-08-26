cd9c043f88265fc77c36d022fdcb1a5718c8846d
jnprogrammer <joshuanparker>
2017-08-26 04:36:56 -0400

    Added SE9 currently working on highlighting

--- SE9.py
+++ SE9.py
@@ -1,12 +1,10 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 
 browser = webdriver.Chrome()
+actions = ActionChains(browser)
 #browser = webdriver.Firefox()
 FirstName = "Phoenix"
 LastName = "Wright"
